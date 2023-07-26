ca5071661fb9805cc5ec8ae3611bbc41b55d6dae
@FerrerMarkAnthony <markferrer0910@gmail.com>
2023-07-26 20:05:48 +0800

    add views folder

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-  // res.sender("pages/index")
-  res.sendFile(path.join(__dirname, "/pages/index.html"));
+  res.render("pages/index");
+  // res.sendFile(path.join(__dirname, "/pages/index.html"));
 });
 
 app.get("/about", function (req, res) {
-  // res.sender("pages/about")
-  res.sendFile(path.join(__dirname, "/pages/about.html"));
+  res.render("pages/about");
+  // res.sendFile(path.join(__dirname, "/pages/about.html"));
 });
 
 app.listen(3000);
