7184e985ed114ec76fa89fa90a5eb9cdd1d90eb2
@FerrerMarkAnthony <markferrer0910@gmail.com>
2023-07-26 21:23:33 +0800

    final output

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require("express")
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
 
 app.use(express.static("public"));
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
-    res.render("pages/index")
+app.get("/", function (req, res) {
+  res.render("pages/index");
 });
 
-app.get("/about", function(req, res) {
-    res.render("pages/about")
+app.get("/about", function (req, res) {
+  res.render("pages/about");
 });
 
 app.listen(3000);
-console.log("Server is running")
+console.log("Server is running");
