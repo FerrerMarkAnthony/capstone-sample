ef15cb54e74bde46a11fb2b4e96828283048cb46
@FerrerMarkAnthony <markferrer0910@gmail.com>
2023-07-27 19:26:14 +0800

    Adding master Layouts

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
 
 const app = express();
 
+app.use(expressLayouts);
 app.use(express.static("public"));
+
 app.set("view engine", "ejs");
+app.set("layout", "./layouts/master");
 
 app.get("/", function (req, res) {
   res.render("pages/index");
